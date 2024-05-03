3f92da8bc28b69f0be094206fa7c2f790c9fb194
thomas univ-smb <thomas.durand@etu.univ-smb.fr>
2024-05-03 14:02:41 +0000

    Version 2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # save this as app.py
 from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -13,16 +14,28 @@ def concerts():
 
 @app.route("/actua")
 def actua():
-    return render_template('actua.html')
+    f = open('static/actua.json')
+    data = json.load(f) 
+    f.close()
+    return render_template('actua.html', summary = data)
 
 @app.route("/actua_jazz")
 def actua_jazz():
-    return render_template('actua_jazz.html')
+    f = open('static/actua.json')
+    data = json.load(f) 
+    f.close()
+    return render_template('actua_jazz.html', summary = data)
 
 @app.route("/actua_rock")
 def actua_rock():
-    return render_template('actua_rock.html')
+    f = open('static/actua.json')
+    data = json.load(f) 
+    f.close()
+    return render_template('actua_rock.html', summary = data)
 
 @app.route("/actua_electro")
 def actua_electro():
-    return render_template('actua_electro.html')
+    f = open('static/actua.json')
+    data = json.load(f) 
+    f.close()
+    return render_template('actua_electro.html', summary = data)
