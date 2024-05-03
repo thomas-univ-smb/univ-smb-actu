6c1f9ed25391d20c859cfe211a4f84a85733649e
thomas univ-smb <thomas.durand@etu.univ-smb.fr>
2024-05-03 13:10:42 +0000

    Version 1.0

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def template():
+    return render_template('start.html')
+
+@app.route("/concerts")
+def concerts():
+    return render_template('concerts.html')
+
+@app.route("/actua")
+def actua():
+    return render_template('actua.html')
+
+@app.route("/actua_jazz")
+def actua_jazz():
+    return render_template('actua_jazz.html')
+
+@app.route("/actua_rock")
+def actua_rock():
+    return render_template('actua_rock.html')
+
+@app.route("/actua_electro")
+def actua_electro():
+    return render_template('actua_electro.html')
